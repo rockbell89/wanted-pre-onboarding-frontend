6e2e4113ff115bfe6b8fab873ac3088640865735
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-06-06 18:02:31 +0900

    feat: 초기 css 셋팅 및 레이아웃 페이지 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/layouts/Layout';
+import SignUp from './pages/SignUp';
+import './App.scss';
+import SignIn from './pages/SignIn';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: 'signup',
+				element: <SignUp />,
+			},
+			{
+				path: 'signin',
+				element: <SignIn />,
+			},
+		],
+	},
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
