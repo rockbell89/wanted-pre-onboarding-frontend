a157cfdfcc43b3820fdcbfcc526f7938f4525b0f
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-06-07 17:03:09 +0900

    refactor: useInputs 커스텀 훅에 useCallback 적용하여 최적화

--- src/hooks/useInputs.ts
+++ src/hooks/useInputs.ts
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const useInputs = <T extends Record<string, any>>(initialFormData: T) => {
 	const [formData, setFormData] = useState<T>(initialFormData);
 
-	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+	const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = event.target;
 		setFormData((prevState) => ({
 			...prevState,
 			[name]: value,
 		}));
-	};
+	}, []);
 
-	const onReset = () => {
+	const onReset = useCallback(() => {
 		setFormData(initialFormData);
-	};
+	}, [initialFormData]);
 
 	return {
 		formData,
